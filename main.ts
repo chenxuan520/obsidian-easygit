5ed7a738b70fd185f7ccd5a70d68984aa2e696db
chenxuan <1607772321@qq.com>
2023-01-28 13:08:29 +0800

    v1.0

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import {FileSystemAdapter, SuggestModal, App, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting} from 'obsidian';
+import {FileSystemAdapter, SuggestModal, App, Modal, Notice, Plugin, PluginSettingTab, Setting} from 'obsidian';
 import {simpleGit, SimpleGit} from 'simple-git';
 
 // Remember to rename these classes and interfaces!
@@ -22,73 +22,42 @@ export default class ObsidianEasyGit extends Plugin {
 		await this.init();
 
 		if (this.settings.icon) {
-			// This creates an icon in the left ribbon.
-			const ribbonIconEl = this.addRibbonIcon('dice', 'git commit', () => {
-				// Called when the user clicks the icon.
-				this.gitcommit();
+			const ribbonIconEl = this.addRibbonIcon('dice', 'git operator', () => {
+				new IconModal(this.app, this).open()
 			});
-			// Perform additional things with the ribbon
 			ribbonIconEl.addClass('my-plugin-ribbon-class');
 		}
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		/* const statusBarItemEl = this.addStatusBarItem(); */
-		/* statusBarItemEl.setText('easygit is load'); */
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
 			id: 'Git commit',
 			name: 'commit source',
 			callback: () => {
-				this.gitcommit();
-				if (this.settings.afterShell.length!=0) {
-				}
+				easyCommit(this)
+				/* if (this.settings.afterShell.length != 0) { */
+				/* 	exec(this.settings.afterShell) */
+				/* } */
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
 			id: 'Git pull',
 			name: 'pull remote source',
 			callback: () => {
-				this.pull();
-				new Notice('pull success')
+				easyPull(this)
 			}
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
 		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						/* new EasyGitModal(this.app).open(); */
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+			id: 'Git push',
+			name: 'push source to remote',
+			callback: () => {
+				easyPush(this)
 			}
 		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new EasyGitSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
-	}
+	onunload() {}
+
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
@@ -108,31 +77,32 @@ export default class ObsidianEasyGit extends Plugin {
 			config: ["core.quotepath=off"]
 		});
 	}
+}
 
-	async pull() {
-		this.git.pull()
-	}
+function easyCommit(app: ObsidianEasyGit) {
+	new EasyGitModal(app.app, (text) => {
+		const msg = new Date().toLocaleString()
+		app.git.add(".")
+		app.git.commit(text + " " + msg, (err) => {
+			if (err != null) {
+				new Notice("Commit wrong:" + err.message)
+			} else {
+				new Notice("Commit ok")
+				easyPush(app)
+			}
+		})
+	}).open();
+}
 
-	async push() {
-		this.git.push()
-	}
+function easyPull(app: ObsidianEasyGit) {
+	app.git.pull()
+	new Notice('pull success')
+}
 
-	async gitcommit() {
-		new EasyGitModal(this.app, (text) => {
-			const msg = new Date().toLocaleString()
-			this.git.add(".")
-			this.git.commit(text + " " + msg, (err) => {
-				if (err != null) {
-					new Notice("Commit wrong:" + err.message)
-				} else {
-					new Notice("Commit ok")
-				}
-			})
-		}).open();
-		if (this.git.remote.length != 0) {
-			this.git.push()
-			new Notice("Push ok")
-		}
+function easyPush(app: ObsidianEasyGit) {
+	if (app.git.remote.length != 0) {
+		app.git.push()
+		new Notice("Push ok")
 	}
 }
 
@@ -189,24 +159,25 @@ class EasyGitSettingTab extends PluginSettingTab {
 
 		containerEl.createEl('h2', {text: 'Settings for easygit.'});
 
-		new Setting(containerEl)
-			.setName('AfterShell')
-			.setDesc('run shell after exec commit')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.afterShell)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.afterShell = value;
-					await this.plugin.saveSettings();
-				}));
+		/* new Setting(containerEl) */
+		/* 	.setName('AfterShell') */
+		/* 	.setDesc('run shell after exec commit') */
+		/* 	.addText(text => text */
+		/* 		.setPlaceholder('Enter your secret') */
+		/* 		.setValue(this.plugin.settings.afterShell) */
+		/* 		.onChange(async (value) => { */
+		/* 			console.log('Secret: ' + value); */
+		/* 			this.plugin.settings.afterShell = value; */
+		/* 			await this.plugin.saveSettings(); */
+		/* 		})); */
 
 		new Setting(containerEl)
 			.setName('IconOpen')
 			.setDesc('is icon open,default true')
-			.addToggle(tog => tog.setValue(true).onChange(
+			.addToggle(tog => tog.setValue(this.plugin.settings.icon).onChange(
 				async (value) => {
 					this.plugin.settings.icon = value;
+					await this.plugin.saveSettings();
 				}
 			))
 	}
@@ -214,30 +185,30 @@ class EasyGitSettingTab extends PluginSettingTab {
 
 interface Oper {
 	operator: string;
-	/* callback: ()=>void; */
+	callback: (arg0: ObsidianEasyGit) => void;
 }
 
-const ALL_BOOKS = [
-	{operator: "git commit", },
-	{operator: "git push", },
-	{operator: "git pull", },
-];
-
-export class ExampleModal extends SuggestModal<Oper> {
+export class IconModal extends SuggestModal<Oper> {
+	plugin: ObsidianEasyGit;
 	// Returns all available suggestions.
-	getSuggestions(query: string): Oper[] {
-		return ALL_BOOKS.filter((book) =>
-			book.operator.toLowerCase().includes(query.toLowerCase())
-		);
+	constructor(app: App, plugin: ObsidianEasyGit) {
+		super(app);
+		this.plugin = plugin;
 	}
-
+	getSuggestions(): Oper[] {
+		const opers = [
+			{operator: "commit", callback: easyCommit},
+			{operator: "push", callback: easyPush},
+			{operator: "pull", callback: easyPull},
+		];
+		return opers
+	}
 	// Renders each suggestion item.
 	renderSuggestion(oper: Oper, el: HTMLElement) {
 		el.createEl("div", {text: oper.operator});
 	}
-
 	// Perform action on the selected suggestion.
 	onChooseSuggestion(oper: Oper) {
-		new Notice(`Selected ${oper.operator}`);
+		oper.callback(this.plugin)
 	}
 }
